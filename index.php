b0a0bb3a1cbd462189c593b036b15448f02929c6
C.K. Chan <chanchikin@ied.edu.hk>
2015-06-15 10:20:10 +0800

    start

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include 'config/config.php';?>
+<?php include 'libraries/Database.php'; ?>
+<?php include 'includes/header.php'; ?>
+<?php
+	//Create DB Object
+?>
+
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">International PHP Conference</h2>
+            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+            <p>As you sit down to eat your lunchtime sandwich today, these pictures might give you pause for thought.
+Spreading ingredients onto countless slices of bread, the rows of workers at a Midlands industrial plant are part of an operation that makes a staggering three million sandwiches a week, selling them at chain stores ranging from Waitrose, M&S and the Co-op to Sainsburyâ€™s, Asda and Boots.
+The fact that the workers use bare hands at Britainâ€™s biggest sandwich-making factory might at first glance put people off their food.But Greencore, the company that owns it, insists that this makes it easier for the 2,000 employees to detect any potential â€˜contaminationâ€?on their skin â€?a rogue scrap of chicken breast, for example, when the production line has switched to cheese sandwiches.
+Read more: http://www.dailymail.co.uk/news/article-3114625/How-make-lunchtime-sarnie-s-hands-Britain-s-biggest-sandwich-factory-migrant-workers-help-churn-THREE-MILLION-week.html#ixzz3cRuQ6uYq 
+Follow us: @MailOnline on Twitter | DailyMail on Facebook</p>
+            
+			<a class="readmore"	href="post.php?id=1">Read More</a>
+          </div><!-- /.blog-post -->
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">PHP Beta released</h2>
+            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+<p>As you sit down to eat your lunchtime sandwich today, these pictures might give you pause for thought.
+Spreading ingredients onto countless slices of bread, the rows of workers at a Midlands industrial plant are part of an operation that makes a staggering three million sandwiches a week, selling them at chain stores ranging from Waitrose, M&S and the Co-op to Sainsburyâ€™s, Asda and Boots.
+The fact that the workers use bare hands at Britainâ€™s biggest sandwich-making factory might at first glance put people off their food.But Greencore, the company that owns it, insists that this makes it easier for the 2,000 employees to detect any potential â€˜contaminationâ€?on their skin â€?a rogue scrap of chicken breast, for example, when the production line has switched to cheese sandwiches.
+Read more: http://www.dailymail.co.uk/news/article-3114625/How-make-lunchtime-sarnie-s-hands-Britain-s-biggest-sandwich-factory-migrant-workers-help-churn-THREE-MILLION-week.html#ixzz3cRuQ6uYq 
+Follow us: @MailOnline on Twitter | DailyMail on Facebook</p>
+			<a class="readmore"	href="post.php?id=1">Read More</a>
+          </div><!-- /.blog-post -->
+          <?php include 'includes/footer.php'; ?>
+
+          
+
+         
---
+++ post.php
@@ -0,0 +1,24 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="blog-post">
+            <h2 class="blog-post-title">International PHP Conference</h2>
+            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+            <p>As you sit down to eat your lunchtime sandwich today, these pictures might give you pause for thought.
+Spreading ingredients onto countless slices of bread, the rows of workers at a Midlands industrial plant are part of an operation that makes a staggering three million sandwiches a week, selling them at chain stores ranging from Waitrose, M&S and the Co-op to Sainsburyâ€™s, Asda and Boots.
+The fact that the workers use bare hands at Britainâ€™s biggest sandwich-making factory might at first glance put people off their food.But Greencore, the company that owns it, insists that this makes it easier for the 2,000 employees to detect any potential â€˜contaminationâ€?on their skin â€?a rogue scrap of chicken breast, for example, when the production line has switched to cheese sandwiches.
+Read more: http://www.dailymail.co.uk/news/article-3114625/How-make-lunchtime-sarnie-s-hands-Britain-s-biggest-sandwich-factory-migrant-workers-help-churn-THREE-MILLION-week.html#ixzz3cRuQ6uYq 
+Follow us: @MailOnline on Twitter | DailyMail on Facebook</p>
+            
+			<p>The work can be repetitive and, during a 6am to 2pm shift, staff receive a 45-minute break, but are paid for only 25 minutes of this.
+The Dublin-based company said only one per cent staff were on the minimum wage ¡ª rising to ten per cent across the Group.
+Greencore claimed there had been no redundancies at Manton Wood for over ten years and announced it is creating 70 new jobs on the site as the company prospers. It said more than 100 employees had worked at the factory for more than 20 years and only one grievance had been raised in the past six months.
+¡®We are proud that we have an engaged, loyal and highly skilled set of colleagues at every layer of the business,¡¯ said a spokesman, ¡®and we have worked very hard to create a culture that focuses on personal development and puts our people at the very core of everything that we do.¡¯
+The company says Greencore has a ¡®flexible workforce¡¯, adding: ¡®While an agency employee would typically expect to work an eight-hour shift, they are sometimes asked to stay a little longer to complete a task. The reverse is also true, and agency employees occasionally leave earlier than expected if a task has been completed before the end of their shift.
+
+
+Read more: http://www.dailymail.co.uk/news/article-3114625/How-make-lunchtime-sarnie-s-hands-Britain-s-biggest-sandwich-factory-migrant-workers-help-churn-THREE-MILLION-week.html#ixzz3cWowOdtm 
+Follow us: @MailOnline on Twitter | DailyMail on Facebook</p>
+          </div><!-- /.blog-post -->
+
+
+<?php include 'includes/footer.php'; ?>
