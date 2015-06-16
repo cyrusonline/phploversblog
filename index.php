c8d0ecd227e50319477c7b7a05b6a5b890ef36a2
C.K. Chan <chanchikin@ied.edu.hk>
2015-06-16 11:40:18 +0800

    fetch from database 14:15

--- index.php
+++ index.php
@@ -1,35 +1,36 @@
 <?php include 'config/config.php';?>
 <?php include 'libraries/Database.php'; ?>
 <?php include 'includes/header.php'; ?>
+<?php include 'helpers/format_helper.php'?>
 <?php
 	//Create DB Object
 	$db = new DataBase();
 	
+	//Create Query
+	$query = "SELECT * FROM posts";
+	
+	//Run Query
+	$posts = $db->select($query);
+	
+	
+	
 ?>
-
-
-          <div class="blog-post">
-            <h2 class="blog-post-title">International PHP Conference</h2>
-            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-            <p>As you sit down to eat your lunchtime sandwich today, these pictures might give you pause for thought.
-Spreading ingredients onto countless slices of bread, the rows of workers at a Midlands industrial plant are part of an operation that makes a staggering three million sandwiches a week, selling them at chain stores ranging from Waitrose, M&S and the Co-op to Sainsburyâ€™s, Asda and Boots.
-The fact that the workers use bare hands at Britainâ€™s biggest sandwich-making factory might at first glance put people off their food.But Greencore, the company that owns it, insists that this makes it easier for the 2,000 employees to detect any potential â€˜contaminationâ€?on their skin â€?a rogue scrap of chicken breast, for example, when the production line has switched to cheese sandwiches.
-Read more: http://www.dailymail.co.uk/news/article-3114625/How-make-lunchtime-sarnie-s-hands-Britain-s-biggest-sandwich-factory-migrant-workers-help-churn-THREE-MILLION-week.html#ixzz3cRuQ6uYq 
-Follow us: @MailOnline on Twitter | DailyMail on Facebook</p>
-            
-			<a class="readmore"	href="post.php?id=1">Read More</a>
-          </div><!-- /.blog-post -->
+<?php if($posts):?>
+<?php while($row = $posts->fetch_assoc()):?>
 
           <div class="blog-post">
-            <h2 class="blog-post-title">PHP Beta released</h2>
-            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
-<p>As you sit down to eat your lunchtime sandwich today, these pictures might give you pause for thought.
-Spreading ingredients onto countless slices of bread, the rows of workers at a Midlands industrial plant are part of an operation that makes a staggering three million sandwiches a week, selling them at chain stores ranging from Waitrose, M&S and the Co-op to Sainsburyâ€™s, Asda and Boots.
-The fact that the workers use bare hands at Britainâ€™s biggest sandwich-making factory might at first glance put people off their food.But Greencore, the company that owns it, insists that this makes it easier for the 2,000 employees to detect any potential â€˜contaminationâ€?on their skin â€?a rogue scrap of chicken breast, for example, when the production line has switched to cheese sandwiches.
-Read more: http://www.dailymail.co.uk/news/article-3114625/How-make-lunchtime-sarnie-s-hands-Britain-s-biggest-sandwich-factory-migrant-workers-help-churn-THREE-MILLION-week.html#ixzz3cRuQ6uYq 
-Follow us: @MailOnline on Twitter | DailyMail on Facebook</p>
+          <p><?php echo $row['date'];?></p>
+            <h2 class="blog-post-title"><?php echo $row['title'];?></h2>
+            <p class="blog-post-meta"><?php echo formatDate($row['date']);?> by <a href="#"><?php echo $row['author']?></a></p>
+            <?php echo shortenText($row['body']);?>
 			<a class="readmore"	href="post.php?id=1">Read More</a>
           </div><!-- /.blog-post -->
+<?php endwhile;?>
+          
+      
+          <?php else:?>
+          <p>There are no posts yet</p>
+          <?php endif;?>
           <?php include 'includes/footer.php'; ?>
 
           
