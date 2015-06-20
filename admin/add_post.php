51dd8fa12b7029c01099da63a90f71dcf32daf69
C.K.Chan <ck.chan1225@yahoo.com.hk>
2015-06-20 10:24:58 +0800

    insert post and categories 11:23

--- admin/add_post.php
+++ admin/add_post.php
@@ -10,7 +10,19 @@ if(isset($_POST['submit'])){
 	$category = mysqli_real_escape_string($db->link, $_POST['category']);
 	$author = mysqli_real_escape_string($db->link, $_POST['author']);
 	$tags = mysqli_real_escape_string($db->link, $_POST['tags']);
+	//Simple validation
+	if ($title == ''||$body == ''||$category == ''||$author==''){
+		//Set error
+		$error = 'Please fill out all required field';
 		
+	}else {
+		
+		$query = "INSERT INTO posts
+				 (title, body, category, author, tags)
+				VALUES('$title', '$body',$category,'$author','$tags')";
+				}
+		
+		$insert_row = $db->insert($query);
 }
 
 ?>
@@ -50,7 +62,7 @@ $categories = $db->select ( $query );
     	
     	?>
     	
-	  <option <?php echo $selected?>><?php echo $row['name']?></option>
+	  <option <?php echo $selected?> value="<?php echo $row['id']?>"><?php echo $row['name']?></option>
 	  
 	  <?php endwhile;?>
 
